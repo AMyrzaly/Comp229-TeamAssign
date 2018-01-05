ba218e1a081b515615dc2be1b47c1a81455c7c65
Imba-lol <best.abuka@gmail.com>
2018-01-05 17:53:22 -0500

    Added contact.aspx and button event

--- Comp229-Team-Assign/contact.aspx
+++ Comp229-Team-Assign/contact.aspx
@@ -1,6 +1,37 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/main.Master" AutoEventWireup="true" CodeBehind="contact.aspx.cs" Inherits="Comp229_Team_Assign.contact" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="mainContain" runat="server">
-    
+
+    <div class="container-fluid bg-grey">
+        <h2 class="text-center">CONTACT</h2>
+        <div class="row">
+            <div class="col-sm-5">
+                <p>Contact us and we'll get back to you within 24 hours.</p>
+                <p><span class="glyphicon glyphicon-map-marker"></span> Toronto, Canada</p>
+                <p><span class="glyphicon glyphicon-phone"></span>+1(647) 1515151515</p>
+                <p><span class="glyphicon glyphicon-envelope"></span> globalhealthcare@gmail.com</p>
+            </div>
+            <div class="col-sm-7">
+                <div class="row">
+                    <div class="col-sm-6 form-group">
+                        <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                    </div>
+                    <div class="col-sm-6 form-group">
+                        <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                    </div>
+                </div>
+                <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+                <div class="row">
+                    <div class="col-sm-12 form-group">
+                        <button class="btn btn-default pull-right" type="submit">
+                            <asp:Button ID="Button1" runat="server" Font-Bold="True" Height="36px" OnClick="Button1_Click" Text="Send" Width="91px" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </asp:Content>
--- Comp229-Team-Assign/contact.aspx.cs
+++ Comp229-Team-Assign/contact.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +13,43 @@ namespace Comp229_Team_Assign
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Set email user name - Change this as per your settings
+            const string username = "testuser@googlemail.com";
+            //Set the email password - Change this as per your settings
+            const string password = "xxxxxxxxx";
+            SmtpClient smtpclient = new SmtpClient();
+            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
+            //Set the email from address of mail message -  Change this as per your settings
+            MailAddress fromaddress = new MailAddress("testuser@googlemail.com");
+            //Set the email smtp host -  Change this as per your settings
+            smtpclient.Host = "smtp.gmail.com";
+            //Set the email client port -  Change this as per your settings
+            smtpclient.Port = 587;
+            mail.From = fromaddress;
+            //Adding email id of receiver link
+            mail.To.Add("testuser@googlemail.com");
+            //Set the email subject
+            mail.Subject = ("Test Subject");
+            mail.IsBodyHtml = true;
+            //Set the email body - Change this as per your logic
+            mail.Body = "This is test Mail";
+            smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpclient.Credentials = new System.Net.NetworkCredential(username, password);
+            try
+            {
+                //Sending Email
+                smtpclient.Send(mail);
+                Response.Write("<B>Email Has been sent successfully.</B>");
+            }
+            catch (Exception ex)
+            {
+                //Catch if any exception occurs
+                Response.Write(ex.Message);
+            }
         }
     }
 }
